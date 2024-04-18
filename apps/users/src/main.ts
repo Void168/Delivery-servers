9202d7dd0fb9e2c6b351db614449daf5203121d2
LPHung <phuhung16820@gmail.com>
2024-04-19 02:06:14 +0700

    cors

--- apps/users/src/main.ts
+++ apps/users/src/main.ts
@@ -11,6 +11,10 @@ async function bootstrap() {
   app.setBaseViewsDir(join(__dirname, '..', 'servers/email-templates'))
   app.setViewEngine('ejs')
   
+  app.enableCors({
+    origin: '*',
+  });
+
   await app.listen(4001);
 }
 bootstrap();
